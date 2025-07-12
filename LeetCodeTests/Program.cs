abcff78e480f9092193027daec40223f5154bf3c
Kwameldx666 <artemios.sologan@mib.utm.md>
2025-07-12 15:57:53 +0300

    Replace Gas_Station with CandyGame for candy distribution
    Updated `Program.cs` to use `CandyGame` for calculating total candies based on ratings. Added a new `CandyGame` class in `Candy.cs` with a `Candy` method that implements the candy distribution logic. Included necessary using directives and examples in comments for clarity.

--- LeetCodeTests/Program.cs
+++ LeetCodeTests/Program.cs
@@ -8,11 +8,9 @@ namespace LeetCodeTests.Task4
     {
         static void Main(string[] args)
         {
-            Gas_Station gasstation = new Gas_Station();
-            int[] gas = new int[] { 5, 8, 2, 8 };
-            int[] cost = new int[] { 6, 5, 6, 6 };
-
-            Console.WriteLine(gasstation.CanCompleteCircuit(gas, cost));
+            CandyGame game = new CandyGame();
+            int[] ratings = { 1, 0, 2 };
+            Console.WriteLine(game.Candy(ratings));
 
 
 
---
+++ LeetCodeTests/Task/Array and String/Candy.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Example 1:
+
+Input: ratings = [1, 0, 2]
+Output: 5
+Explanation: You can allocate to the first, second and third child with 2, 1, 2 candies respectively.
+Example 2:
+
+Input: ratings = [1, 2, 2]
+Output: 4
+Explanation: You can allocate to the first, second and third child with 1, 2, 1 candies respectively.
+The third child gets 1 candy because it satisfies the above two conditions.*/
+namespace LeetCodeTests.Task.Array_and_String
+{
+    internal class CandyGame
+    {
+        public int Candy(int[] ratings)
+        {
+            int[] candies = new int[ratings.Length];
+            Array.Fill(candies, 1);
+            int n = ratings.Length - 2;
+            for(int i = 0; i <= n; i++)
+            {
+                if (ratings[i] < ratings[i + 1])
+                {
+                    candies[i + 1] = candies[i] + 1; 
+                }            
+            }
+            for (int i = n; i >= 0; i--)
+            {
+                if (ratings[i] > ratings[i + 1])
+                {
+                    candies[i] = int.Max(candies[i + 1] + 1, candies[i]);
+                }
+            }
+            return candies.Sum();
+        }
+    }
+}
+
